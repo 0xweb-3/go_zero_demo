d3fe37010bb79b4f53a3187a99b55d01cc037eff
xin <709464835@qq.com>
2025-06-17 14:04:50 +0800

    rpc init

---
+++ demo/user/rpc/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "github.com/0xweb-3/go_zero_demo/demo/user/rpc/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
