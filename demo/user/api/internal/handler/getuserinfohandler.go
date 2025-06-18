48df47db1d07c3f6c6d79775b0e69a182f8ddcdd
xin <709464835@qq.com>
2025-06-18 17:00:05 +0800

    api中间件的适用

---
+++ demo/user/api/internal/handler/getuserinfohandler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/0xweb-3/go_zero_demo/demo/user/api/internal/logic"
+	"github.com/0xweb-3/go_zero_demo/demo/user/api/internal/svc"
+	"github.com/0xweb-3/go_zero_demo/demo/user/api/internal/types"
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func getUserInfoHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.UserReq
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := logic.NewGetUserInfoLogic(r.Context(), svcCtx)
+		resp, err := l.GetUserInfo(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
--- demo/user/api/internal/svc/servicecontext.go
+++ demo/user/api/internal/svc/servicecontext.go
@@ -2,7 +2,9 @@ package svc
 
 import (
 	"github.com/0xweb-3/go_zero_demo/demo/user/api/internal/config"
+	"github.com/0xweb-3/go_zero_demo/demo/user/api/internal/middleware"
 	"github.com/0xweb-3/go_zero_demo/demo/user/rpc/userclient"
+	"github.com/zeromicro/go-zero/rest"
 	"github.com/zeromicro/go-zero/zrpc"
 )
 
@@ -10,6 +12,8 @@ type ServiceContext struct {
 	Config config.Config
 
 	userclient.User
+
+	LoginVerification rest.Middleware // 在demo/user/api/internal/handler/routes.go下对应的名称
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
@@ -17,5 +21,8 @@ func NewServiceContext(c config.Config) *ServiceContext {
 		Config: c,
 
 		User: userclient.NewUser(zrpc.MustNewClient(c.UserRpc)),
+
+		// 配置中间件
+		LoginVerification: middleware.NewLoginVerificationMiddleware().Handle,
 	}
 }
