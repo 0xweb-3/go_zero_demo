8e5ac96f18b098f32971e68b54a6d23ee8d2d4d5
xin <709464835@qq.com>
2025-06-27 16:13:23 +0800

    用户api服务部署

--- easychat/deploy/script/user-rpc-test.sh
+++ easychat/deploy/script/user-rpc-test.sh
@@ -2,6 +2,8 @@
 reso_addr='crpi-8mkzdazjkdk1gs4d.cn-hangzhou.personal.cr.aliyuncs.com/xin_go_zero/user-rpc-dev'
 tag='latest'
 
+pod_ip="192.168.1.247" # 用于设置外部访问服务ip
+
 container_name="easy-chat-user-rpc-test"
 
 docker stop ${container_name}
@@ -15,4 +17,4 @@ docker pull ${reso_addr}:${tag}
 
 # 如果需要指定配置文件的
 # docker run -p 10001:8080 --network imooc_easy-chat -v /easy-chat/config/user-rpc:/user/conf/ --name=${container_name} -d ${reso_addr}:${tag}
-docker run -p 10001:8080  --name=${container_name} -d ${reso_addr}:${tag}
+docker run -p 10001:10001 -e POD_IP=${pod_ip}  --name=${container_name} -d ${reso_addr}:${tag}
