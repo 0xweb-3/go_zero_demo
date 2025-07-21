2f7d2e904165c046ced9aa592b2bbeed1d1ef291
xin <709464835@qq.com>
2025-07-21 17:37:52 +0800

    实现elk日志收集

--- demo/jeager/jeager_test.go
+++ demo/jeager/jeager_test.go
@@ -1,1 +1,49 @@
 package jeager
+
+import (
+	"fmt"
+	"github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go"
+	jeagerconfig "github.com/uber/jaeger-client-go/config"
+	"testing"
+)
+
+func Test_Jeager(t *testing.T) {
+	cfg := jeagerconfig.Configuration{
+		// 定义取样器
+		Sampler: &jeagerconfig.SamplerConfig{
+			Type:  jaeger.SamplerTypeConst, // 取样器类型
+			Param: 1,
+		},
+		// 采样信息发送的对象
+		Reporter: &jeagerconfig.ReporterConfig{
+			LogSpans:          true,
+			CollectorEndpoint: fmt.Sprintf("http://%s/api/traces", "127.0.0.1:14268"),
+		},
+	}
+
+	// 创建jeager客户端
+	jaeger, err := cfg.InitGlobalTracer("client test", jeagerconfig.Logger(jaeger.StdLogger))
+
+	if err != nil {
+		t.Log(err)
+		return
+	}
+	defer jaeger.Close()
+
+	// 任务的执行
+	// 得到tracer
+	tracer := opentracing.GlobalTracer()
+
+	// 为任务节点定义span
+	parentSpan := tracer.StartSpan("A")
+	defer parentSpan.Finish()
+
+	B(tracer, parentSpan)
+}
+
+// 子集任务
+func B(tracer opentracing.Tracer, parentSpan opentracing.Span) {
+	childSpan := tracer.StartSpan("B", opentracing.ChildOf(parentSpan.Context()))
+	defer childSpan.Finish()
+}
--- go.mod
+++ go.mod
@@ -62,6 +62,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/openzipkin/zipkin-go v0.4.3 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
@@ -79,6 +80,8 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.13.0 // indirect
 	github.com/subosito/gotenv v1.4.1 // indirect
+	github.com/uber/jaeger-client-go v2.30.0+incompatible // indirect
+	github.com/uber/jaeger-lib v2.4.1+incompatible // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
